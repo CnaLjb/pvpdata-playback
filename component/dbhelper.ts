9c979d93658d7272f0388829368841128deb187c
CnaLjb <409436438@qq.com>
2016-12-07 23:25:08 +0800

    添加promise组件

--- component/dbhelper.ts
+++ component/dbhelper.ts
@@ -3,32 +3,39 @@ var MongoClient = require('mongodb').MongoClient;
 import util = require("util");
 var Promise = require('promise');
 
-class DBHelper{
+export class DBHelper{
 
     private static dbcon;
 
     static getConnection(ip:string):any{
-        if(!DBHelper.dbcon){
-            var url = util.format("mongodb://%s/poke302",ip);
-            MongoClient.connect(url)
-            .then(function (db) { // <- db as first argument
-                console.log(db);
-                DBHelper.dbcon = db;
-            })
-            .catch(function (err) {
-                console.log("link db error:\t",err);
-            })
-        }
-        
-        return DBHelper.dbcon
+        return new Promise(function(resolve,reject){
+            if(!DBHelper.dbcon){
+                var url = util.format("mongodb://%s/poke302",ip);
+                MongoClient.connect(url)
+                .then(function (db) { // <- db as first argument
+                    DBHelper.dbcon = db;
+                    resolve(DBHelper.dbcon);
+                })
+                .catch(function (err) {
+                    console.log("link db error");
+                    reject(err);
+                })
+            }else{
+                resolve(DBHelper.dbcon);
+            }
+        })
     }
 
-    static getPvpRecord(){
-        return new Promise(function(resolve,reject) {
-
+    static getPvpRecord(userId:string,ip:string){
+        return DBHelper.getConnection(ip).then(function(db){
+            var player = db.collection("player").find({userId:userId});
+            console.log(player.playerData._version);   
         })
     }
 }
 
-
-
+// MongoClient.connect('mongodb://127.0.0.1:27017/poke302', function(err, db){
+//     var col = db.collection('player');
+//     var player = col.find({userId:"1012s302p3734542"})
+//     console.log("player:",player);
+// })
--- rout/rout.ts
+++ rout/rout.ts
@@ -1,7 +1,11 @@
-
-
+import dbhelperMd = require("../component/dbhelper");  
+import DBHelper = dbhelperMd.DBHelper
 
 // https://www.promisejs.org/
+// https://segmentfault.com/a/1190000002928371
+// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Promise
+// https://docs.mongodb.com/manual/tutorial/query-documents/
+
 /**
  * 获取玩家PVP记录
  * 
@@ -12,7 +16,7 @@
 export function getPvplist(req,res):void{
     var userId = req.query.userId;
     var ip = req.query.ip;
-    db.getData()
+    DBHelper.getPvpRecord(userId,ip)
     .then(function (data) {
         res.setHeader('Content-Type', 'text/plain');
         res.end(data);
